7f3eb579f44b6a1b7195b39e80ec6d2e9c9c4448
Abdelali-Moutawassit <abdelalimoutawassit99@gmail.com>
2025-05-27 11:12:31 +0100

    Add Reaction Count to post api

--- src/main/java/abdelali/moutawassit/blogapplication/service/PostReactionService.java
+++ src/main/java/abdelali/moutawassit/blogapplication/service/PostReactionService.java
@@ -32,15 +32,44 @@ public class PostReactionService {
         Post post = postRepository.findById(postId)
                 .orElseThrow(() -> new RuntimeException("Post introuvable"));
 
-        // Vérifier si l'utilisateur a déjà réagi à ce post
         Optional<PostReaction> existingReaction = postReactionRepository.findByUserAndPost(user, post);
-        existingReaction.ifPresent(postReactionRepository::delete); // Supprimer l'ancienne réaction si elle existe
+
+        existingReaction.ifPresent(oldReaction -> {
+            if (oldReaction.getType().name().equals("LIKE")) {
+                post.setLikeCount(post.getLikeCount() - 1); // enlever ancien like
+            }
+            postReactionRepository.delete(oldReaction);
+        });
 
         PostReaction newReaction = PostReactionMapper.toEntity(dto, user, post);
         PostReaction savedReaction = postReactionRepository.save(newReaction);
 
+        // si la nouvelle réaction est un LIKE, on incrémente
+            post.setLikeCount(post.getLikeCount() + 1);
+
+        postRepository.save(post); // très important
+
         return PostReactionMapper.toResponseDTO(savedReaction);
     }
+
+//    public PostReactionResponseDTO addReaction(Long userId, Long postId, PostReactionRequestDTO dto) {
+//        User user = userRepository.findById(userId)
+//                .orElseThrow(() -> new RuntimeException("Utilisateur introuvable"));
+//
+//        Post post = postRepository.findById(postId)
+//                .orElseThrow(() -> new RuntimeException("Post introuvable"));
+//
+////        post.setLikeCount(post.getLikeCount() + 1);
+//
+//        // Vérifier si l'utilisateur a déjà réagi à ce post
+//        Optional<PostReaction> existingReaction = postReactionRepository.findByUserAndPost(user, post);
+//        existingReaction.ifPresent(postReactionRepository::delete); // Supprimer l'ancienne réaction si elle existe
+//
+//        PostReaction newReaction = PostReactionMapper.toEntity(dto, user, post);
+//        PostReaction savedReaction = postReactionRepository.save(newReaction);
+//
+//        return PostReactionMapper.toResponseDTO(savedReaction);
+//    }
 
     @Transactional
     public void deleteReaction(Long userId, Long postId) {
@@ -53,8 +82,24 @@ public class PostReactionService {
         PostReaction reaction = postReactionRepository.findByUserAndPost(user, post)
                 .orElseThrow(() -> new RuntimeException("Réaction introuvable"));
 
+            post.setLikeCount(post.getLikeCount() - 1);
+
         postReactionRepository.delete(reaction);
+        postRepository.save(post); // très important
     }
+
+//    public void deleteReaction(Long userId, Long postId) {
+//        User user = userRepository.findById(userId)
+//                .orElseThrow(() -> new RuntimeException("Utilisateur introuvable"));
+//
+//        Post post = postRepository.findById(postId)
+//                .orElseThrow(() -> new RuntimeException("Post introuvable"));
+//
+//        PostReaction reaction = postReactionRepository.findByUserAndPost(user, post)
+//                .orElseThrow(() -> new RuntimeException("Réaction introuvable"));
+//
+//        postReactionRepository.delete(reaction);
+//    }
 
     public List<PostReactionResponseDTO> getReactionsForPost(Long postId) {
         Post post = postRepository.findById(postId)
